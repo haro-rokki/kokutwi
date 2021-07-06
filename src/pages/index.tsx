4cb268ddfb643fef9d025e68fa39ccd3cde7ad0e
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2021-07-06 19:07:14 +0900

    Chakra UIを導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { NextPage } from 'next';
-
 import { Tweet } from 'react-twitter-widgets';
+import { Flex, Box, Heading, Spacer, Button } from '@chakra-ui/react';
 
 const Index: NextPage = () => {
   return (
     <>
-      <p>This is Kokutwi.</p>
+      <Flex>
+        <Box p="2">
+          <Heading size="md">Kokutwi</Heading>
+        </Box>
+        <Spacer />
+        <Box>
+          <Button colorScheme="teal" mr="4">
+            Sign Up
+          </Button>
+          <Button colorScheme="teal">Log in</Button>
+        </Box>
+      </Flex>
       <Tweet tweetId="841418541026877441" options={{ width: '200' }} />
     </>
   );
