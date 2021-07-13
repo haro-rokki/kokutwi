43a18bc998d4368d304d29a52409403a9f23ff58
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2021-07-13 22:23:27 +0900

    react-calendarをインストール

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 import { Tweet } from 'react-twitter-widgets';
 import { Flex, Box, Heading, Spacer, Button } from '@chakra-ui/react';
 import useSWR from 'swr';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 import { getKokutweet } from 'domains/firebase/services/get-kokutweet';
 
 const Index: NextPage = () => {
+  const [calendarValue, onChangeCalendarValue] = useState(new Date());
+  const [tweetId, setTweetId] = useState('202107/3/1410473327328522244');
   const { data: kokutweet } = useSWR(
-    'public/v1/kokutwi/202107/3/1410473327328522244',
+    `public/v1/kokutwi/${tweetId}`,
     getKokutweet,
     {
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
     },
   );
-  console.log(kokutweet);
 
   return (
     <>
@@ -30,7 +34,17 @@ const Index: NextPage = () => {
           <Button colorScheme="teal">Log in</Button>
         </Box>
       </Flex>
-      <Tweet tweetId="1414874714015555588" options={{ width: '300' }} />
+      <Calendar
+        locale="ja-JP"
+        calendarType="US"
+        onChange={onChangeCalendarValue}
+        value={calendarValue}
+        onClickDay={() => {
+          setTweetId('202108/1/1412241294554898433');
+        }}
+      />
+      <p>{kokutweet?.eventDate.seconds}</p>
+      <Tweet tweetId="1412241294554898433" options={{ width: '300' }} />
     </>
   );
 };
